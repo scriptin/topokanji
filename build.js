e8c1b5c4c79109f82a7a852e1941f42181c77eea
Dmitry Shpika <scrptn@gmail.com>
2015-05-28 20:10:12 +0300

    Implement actual building, major refactoring

--- build.js
+++ build.js
@@ -4,12 +4,16 @@
 
 var
   _ = require('lodash'),
+  fs = require('fs'),
   argv = require('minimist')(process.argv.slice(2)),
   cjk = require('./lib/cjk'),
+  deps = require('./lib/deps'),
   kanji = require('./lib/kanji'),
-  dag = require('./lib/dag');
+  dag = require('./lib/dag'),
+  format = require('./lib/format');
 
 var // directories
+  FINAL_LISTS_DIR = './lists/',
   DATA_DIR = './data/',
   FREQ_TABLES_DIR = DATA_DIR + 'kanji-frequency/',
   KANJIVG_SVG_DIR = './kanjivg/kanji/';
@@ -20,84 +24,65 @@ var // files
   CJK = DATA_DIR + 'cjk-decomp-0.4.0.txt',
   CJK_OVERRIDE = DATA_DIR + 'cjk-decomp-override.txt';
 
-var EMPTY_CHAR = '0';
-
 var ARGS = {
   charsPerLine: 'chars-per-line',
-  useFreqTable: 'use-freq-table'
+  useFreqTable: 'use-freq-table',
+  overrideFinalLists: 'override-final-lists'
 };
 
-console.log('Reading kanji lists and frequency table...');
-var useFreqTable = argv[ARGS.useFreqTable] || 'aozora';
-var kanjiData = kanji.readFromFile(
-  KANJI_LIST, KANJIVG_LIST, 
-  FREQ_TABLES_DIR + useFreqTable + '.json'
-);
+console.log('Reading kanji lists...');
+var kanjiData = kanji.readKanjiData(KANJI_LIST, KANJIVG_LIST);
 
 console.log('Reading CJK decompositions...');
 var decompositions = cjk.readFromFile(CJK_OVERRIDE, cjk.readFromFile(CJK));
 
-function decompose(char, decompositions, terminalChars) {
-  if (_.isUndefined(decompositions[char]) || _.isEmpty(decompositions[char])) {
-    return char;
-  }
-  return decompositions[char].map(function (c) {
-    if (_.contains(terminalChars, c) || (c === EMPTY_CHAR)) {
-      return c;
-    }
-    return decompose(c, decompositions, terminalChars);
-  });
-}
-
-var decomposeFlat = _.flow(decompose, _.flattenDeep, _.uniq);
-
 console.log('Building list of dependencies...');
-var dependencies = _.chain(kanjiData.list)
-  .map(function (char) {
-    return decomposeFlat(char, decompositions, kanjiData.list).map(function (part) {
-      return [char, part];
-    });
-  })
-  .flatten()
-  .value();
+var dependencies = deps.buildDependencies(kanjiData.list, decompositions);
 
-console.log('Looking for missing/broken dependencies...');
-var missing = dependencies.filter(function (dep) {
-  return !(dep[1] === EMPTY_CHAR || _.contains(kanjiData.list, dep[1]));
-});
-
-if (missing.length > 0) {
-  console.log('MISSING DEPENDENCIES:');
-  missing.forEach(function (dep) {
-    console.log(dep.join(' -> '));
-  });
-  throw new Error('Fix mising dependencies and retry');
-}
-
-var maxFreq = kanjiData.freqTable[1][2]; // 1st row is all kanji, 2nd is the most used one
 var maxStrokeCount = _.max(kanjiData.strokeCount);
-function weight(char) {
-  var
-    f = 1.0 - (kanjiData.frequency[char] || 0.0) / maxFreq,
-    s = kanjiData.strokeCount[char] / maxStrokeCount;
-  return s * f;
+
+function buildWeightFinction(freqData) {
+  var maxFreq = freqData.freqTable[1][2]; // 1st row is all kanji, 2nd is the most used one
+  return function (char) {
+    var
+      f = 1.0 - (freqData.frequency[char] || 0.0) / maxFreq,
+      s = kanjiData.strokeCount[char] / maxStrokeCount;
+    return s * f;
+  }
 }
 
-console.log('Sorting...');
-var sorted = _.without(dag.toposort(dependencies, weight), EMPTY_CHAR).reverse();
+function buildList(freqTableName) {
+  var freqTableFileName = FREQ_TABLES_DIR + freqTableName + '.json';
+  console.log('Reading kanji usage frequency data from ' + freqTableFileName + ' ...');
+  var freqData = kanji.readFreqData(freqTableFileName);
+  var weightFuntion = buildWeightFinction(freqData);
 
-console.log(sorted.length + ' characters in the final list');
-
-var charsPerLine = argv[ARGS.charsPerLine] || 50;
-
-function splitInLines(chars, charsPerLine) {
-  return _.chunk(chars, charsPerLine)
-    .map(function (row) {
-      return row.join('');
-    }).join('\n');
+  return _.without(
+    dag.toposort(dependencies, weightFuntion),
+    cjk.EMPTY_CHAR
+  ).reverse();
 }
 
-console.log('RESULT:');
-console.log(splitInLines(sorted, charsPerLine));
+if (argv[ARGS.overrideFinalLists]) {
+
+  fs.readdirSync(FREQ_TABLES_DIR).forEach(function (fileName) {
+    var freqTableName = fileName.replace('.json', '');
+    var listFileName = FINAL_LISTS_DIR + freqTableName + '.txt';
+    console.log('Building final list: ' + listFileName + ' ...');
+    var finalList = buildList(freqTableName);
+    fs.writeFileSync(listFileName, format.splitInLines(finalList, 10));
+  });
+
+} else { // not building yet
+  
+  var freqTableName = argv[ARGS.useFreqTable] || 'aozora';
+  var charsPerLine = argv[ARGS.charsPerLine] || 50;
+  var finalList = buildList(freqTableName);
+
+  console.log('RESULT:');
+  console.log(format.splitInLines(finalList, charsPerLine));
+  console.log('(' + finalList.length + ' characters)');
+
+}
 
 console.log('DONE');
