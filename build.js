c0c3c2c50455317b437e0b23af041100ba5c8e22
Dmitry Shpika <scrptn@gmail.com>
2015-06-01 02:23:39 +0300

    Refactor weighting function builder, use explicit argument

--- build.js
+++ build.js
@@ -36,9 +36,8 @@ var decompositions = cjk.readFromFile(CJK_OVERRIDE, cjk.readFromFile(CJK));
 console.log('Building list of dependencies...');
 var dependencies = deps.buildDependencies(kanjiData.list, decompositions);
 
-var maxStrokeCount = _.max(kanjiData.strokeCount);
-
-function buildWeightFinction(freqData) {
+function buildWeightFinction(freqData, kanjiData) {
+  var maxStrokeCount = _.max(kanjiData.strokeCount);
   var maxFreq = freqData.freqTable[1][2]; // 1st row is all kanji, 2nd is the most used one
   return function (char) {
     var
@@ -52,7 +51,7 @@ function buildList(freqTableName) {
   var freqTableFileName = FREQ_TABLES_DIR + freqTableName + '.json';
   console.log('Reading kanji usage frequency data from ' + freqTableFileName + ' ...');
   var freqData = kanji.readFreqData(freqTableFileName);
-  var weightFuntion = buildWeightFinction(freqData);
+  var weightFuntion = buildWeightFinction(freqData, kanjiData);
 
   return _.without(
     dag.toposort(dependencies, weightFuntion),
