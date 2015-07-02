1a3509620638abf963795c6c4a3701787bda00ad
Dmitry Shpika <scrptn@gmail.com>
2015-07-02 14:46:17 +0300

    Move command/argument processing into separate lib file

--- build.js
+++ build.js
@@ -3,9 +3,9 @@
 var
   _ = require('lodash'),
   fs = require('fs'),
-  argv = require('minimist')(process.argv.slice(2)),
   table = require('text-table'),
   cjk = require('./lib/cjk'),
+  command = require('./lib/command'),
   deps = require('./lib/deps'),
   kanji = require('./lib/kanji'),
   kanjiFreq = require('./lib/kanji-freq'),
@@ -29,20 +29,7 @@ var // files
   DEPS_PAIRS = KANJI_DEPS_DIR + '1-to-1.txt',
   DEPS_MAP = KANJI_DEPS_DIR + '1-to-N.txt';
 
-var CMDS = {
-  show: 'show',
-  suggestAdd: 'suggest-add',
-  suggestRemove: 'suggest-remove',
-  coverage: 'coverage',
-  save: 'save'
-};
-
-var ARGS = {
-  num: 'num',
-  perLine: 'per-line',
-  freqTable: 'freq-table',
-  meanType: 'mean-type'
-};
+command.init();
 
 console.log('Reading kanji lists...');
 var kanjiData = kanji.readKanjiData(KANJI_LIST, KANJIVG_LIST, RADICALS_LIST);
@@ -84,12 +71,10 @@ freqTables.push(freqTableAll);
 freqDataSets.all = kanjiFreq.buildFreqData(freqTableAll);
 
 function selectLists(forceAll) {
-  if (forceAll || _.isUndefined(argv[ARGS.freqTable])) {
+  if (forceAll || _.isUndefined(command.getFreqTable())) {
     return Object.keys(freqDataSets);
-  } else if (_.isString(argv[ARGS.freqTable])) {
-    return [ argv[ARGS.freqTable] ];
   }
-  throw new Error('Invalid value for argument --' + ARGS.freqTable + ': ' + argv[ARGS.freqTable]);
+  return [ command.getFreqTable() ];
 }
 
 function getCandidates(coverageData, kanjiData, candidatesCount, removing) {
@@ -111,28 +96,9 @@ function getCandidates(coverageData, kanjiData, candidatesCount, removing) {
     .value();
 }
 
-if (argv._.length > 1) {
-  throw new Error('Only one command can be specified, ' +
-                  argv._.length + ' given: ' + argv._.join(', '));
-}
+if (command.is(command.CMDS.show)) { // displaying list(s)
 
-var unknownCmds = _.difference(argv._, _.values(CMDS));
-if (unknownCmds.length > 0) {
-  throw new Error('Unknown command: ' + unknownCmds.join(', '));
-}
-
-var unknownArgs = _.chain(Object.keys(argv)).without('_').difference(_.values(ARGS)).value();
-if (unknownArgs.length > 0) {
-  throw new Error('Unknown argument(s): ' + unknownArgs.join(', '));
-}
-
-function commandIs(cmd) {
-  return argv._[0] === cmd;
-}
-
-if (commandIs(CMDS.show)) { // displaying list(s)
-
-  var charsPerLine = argv[ARGS.perLine] || 50;
+  var charsPerLine = command.getCharsPerLine();
   selectLists(false).forEach(function (freqTableName) {
     var listFileName = FINAL_LISTS_DIR + freqTableName + '.txt';
     console.log('Building list: ' + freqTableName + ' ...');
@@ -141,7 +107,7 @@ if (commandIs(CMDS.show)) { // displaying list(s)
     console.log(format.splitInLines(finalList, charsPerLine));
   });
 
-} else if (commandIs(CMDS.save)) { // overriding final lists
+} else if (command.is(command.CMDS.save)) { // overriding final lists
 
   var depsMap = deps.buildDependencyMap(dependencies);
   var depsPairs = _.chain(depsMap)
@@ -171,23 +137,18 @@ if (commandIs(CMDS.show)) { // displaying list(s)
     fs.writeFileSync(listFileName, format.splitInLines(finalList, 10));
   });
 
-} else if (commandIs(CMDS.suggestAdd) || commandIs(CMDS.suggestRemove)) { // suggest kanji to add/remove
+} else if (
+  command.is(command.CMDS.suggestAdd) ||
+  command.is(command.CMDS.suggestRemove)
+) { // suggest kanji to add/remove
 
-  var candidatesCount = argv[ARGS.num];
-  if (_.isUndefined(candidatesCount) ||
-      !_.isNumber(candidatesCount) ||
-      candidatesCount < 0 ||
-      (candidatesCount % 1 !== 0)) {
-    throw new Error('Value of --' + ARGS.num + ' must be a positive integer, given ' + candidatesCount);
-  }
-
+  var candidatesCount = command.getNum();
+  var meanType = command.getMeanType();
+  var removing = command.is(command.CMDS.suggestRemove);
   var listNames = _.without(selectLists(true), 'all'); // 'all' list is generated from others
   var tables = listNames.map(function (freqTableName) {
     return freqDataSets[freqTableName].freqTable;
   });
-
-  var meanType = _.get(argv, ARGS.meanType, 'harmonic');
-  var removing = commandIs(CMDS.suggestRemove);
 
   var coverageData = coverage.sort(tables, dependencies, meanType, removing); // when removing, sort in ASC order
   var candidates = getCandidates(coverageData, kanjiData, candidatesCount, removing);
@@ -198,7 +159,7 @@ if (commandIs(CMDS.show)) { // displaying list(s)
               (removing ? 'ASC' : 'DESC') + ':');
   console.log(table([headRow].concat(candidates)));
 
-} else if (commandIs(CMDS.coverage)) {
+} else if (command.is(command.CMDS.coverage)) {
 
   var listNames = selectLists(true);
   var tables = listNames.map(function (freqTableName) {
@@ -208,10 +169,6 @@ if (commandIs(CMDS.show)) { // displaying list(s)
     return [row[0], (row[1] * 100).toFixed(4) + '%'];
   });
   console.log(table([['table', 'coverage']].concat(coverage)));
-
-} else {
-
-  throw new Error('No command provided, use one of these: ' + _.values(CMDS).join(', '));
 
 }
 
