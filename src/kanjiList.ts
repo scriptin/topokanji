2a838462a798323c1b74897b2d7eeb3ed0a17fa3
Dmitry Shpika <scrptn@gmail.com>
2023-03-22 21:46:13 +0400

    Remove kradfile

--- src/kanjiList.ts
+++ src/kanjiList.ts
@@ -1,9 +1,7 @@
 import _ from 'lodash';
 import kanjivgKanji from './kanjivg/kranjivgKanji.js';
 import kanjidic from './kanjidic/kanjidic.js';
-import kradfile from './kradfile/kradfile.js';
 
 const kanjidicKanji = kanjidic.characters.map((c) => c.literal);
-const kradfileKanji = Object.keys(kradfile.kanji);
 
-export default _.intersection(kanjivgKanji, kanjidicKanji, kradfileKanji);
+export default _.intersection(kanjivgKanji, kanjidicKanji);
