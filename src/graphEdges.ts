dbcdb070dc15f301483b92c4df2051bed439888b
Dmitry Shpika <scrptn@gmail.com>
2023-03-23 01:38:20 +0400

    Dependencies based on kradfile with opinionated overrides

--- src/graphEdges.ts
+++ src/graphEdges.ts
@@ -1,16 +1,75 @@
+import kanjiList from './kanjiList.js';
+import kradfile from './kradfile/kradfile.js';
 import { Edge, Node } from './graphTypes.js';
-import dirname from './dirname.js';
-import { readFileSync } from 'fs';
-import { join } from 'path';
-
-const __dirname = dirname(import.meta.url);
 
 export const emptyNode: Node = '0';
 
-const graphEdges = JSON.parse(
-  readFileSync(join(__dirname, '..', 'dependencies', '1-to-1.json'), {
-    encoding: 'utf-8',
-  }),
-) as Edge[];
+const kanji = Object.keys(kradfile.kanji).filter((k) => kanjiList.includes(k));
 
-export default graphEdges;
+const kradfileExclude: Edge[] = [
+  ['口', '囗'], // circular dependency
+  ['毋', '母'], // circular dependency
+  ['刂', '刈'], // dependency is more complex
+  ['鉢', '木'], // should be 本
+  ['氵', '汁'], // dependency is more complex
+];
+
+const kradfileEdges: Edge[] = kanji
+  .reduce((acc: [string, string][], k) => {
+    const decomposition = kradfile.kanji[k];
+    if (!decomposition) return acc;
+    for (const component of decomposition) {
+      acc.push([k, component]);
+    }
+    return acc;
+  }, [])
+  .filter((e) => e[0] !== e[1]) // exclude self-references
+  .filter(
+    // exclude dependencies which create loops or don't make sense
+    (e) =>
+      !kradfileExclude.find(
+        (exclusion) => exclusion[0] === e[0] && exclusion[1] === e[1],
+      ),
+  );
+
+const additionalEdges: Edge[] = [
+  // 1-stroke characters
+  ['一', emptyNode],
+  ['｜', emptyNode], // not a Han character, but useful
+  ['ノ', emptyNode],
+  ['丶', emptyNode],
+  ['亅', emptyNode],
+
+  // fixes for kradfile
+  ['鉢', '本'],
+
+  // opinionated additions to push simpler kanji closer to the beginning
+  ['亠', '一'],
+  ['亠', '丶'],
+  ['子', '亅'],
+  ['山', '凵'],
+  ['目', '日'],
+  ['白', '日'],
+  ['月', '日'],
+  ['日', '口'],
+  ['亻', '｜'],
+  ['彳', '亻'],
+  ['何', '亻'],
+  ['化', '亻'],
+  ['米', '丶'],
+  ['門', '冂'],
+  ['氵', '冫'],
+  ['汁', '氵'],
+  ['汁', '十'],
+  ['刈', '刂'],
+  ['火', '人'],
+  ['迎', '卬'],
+];
+
+const combinedEdges = [...additionalEdges, ...kradfileEdges];
+
+const toEmptyNode: Edge[] = kanji
+  .filter((k) => !combinedEdges.find(([start]) => start === k))
+  .map((k) => [k, emptyNode]);
+
+export default [...toEmptyNode, ...combinedEdges];
--- src/kanjiList.ts
+++ src/kanjiList.ts
@@ -1,7 +1,9 @@
 import _ from 'lodash';
 import kanjivgKanji from './kanjivg/kranjivgKanji.js';
 import kanjidic from './kanjidic/kanjidic.js';
+import kradfile from './kradfile/kradfile.js';
 
 const kanjidicKanji = kanjidic.characters.map((c) => c.literal);
+const kradfileKanji = Object.keys(kradfile.kanji);
 
-export default _.intersection(kanjivgKanji, kanjidicKanji);
+export default _.intersection(kanjivgKanji, kanjidicKanji, kradfileKanji);
